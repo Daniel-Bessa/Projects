22278ba72a862cc4fcac9beccf5828d3ebdfe013
Daniel <daniel.bess22@gmail.com>
2025-10-21 23:57:19 +0000

    A93312:57 AM295-19AMve: ${10/22/2025, 12:57 AMile19}

--- optimize_topnav_full.py
+++ optimize_topnav_full.py
@@ -196,7 +196,7 @@ new_before = '''    /**
     }'''
 
 content = re.sub(old_before, new_before, content)
-print("✓ onCustomWidgetBeforeUpdate optimized")
+print("[OK] onCustomWidgetBeforeUpdate optimized")
 
 # Optimize onCustomWidgetAfterUpdate
 old_after = r'''    //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
