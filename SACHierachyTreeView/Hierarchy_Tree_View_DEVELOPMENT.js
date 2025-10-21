8ebe977b474bf06e171eb5f6b8fadc1d94bcfccf
Daniel <daniel.bess22@gmail.com>
2025-10-21 18:29:24 +0000

    A7737:29 PM294-24PMve: ${10/21/2025, 7:29 PMile24}

--- SACHierachyTreeView/Hierarchy_Tree_View_DEVELOPMENT.js
+++ SACHierachyTreeView/Hierarchy_Tree_View_DEVELOPMENT.js
@@ -2091,7 +2091,6 @@
                                             for (let i = 0; i < uiItems.length; i++) {
                                                 if (uiItems[i]?.mProperties?.selected) { selIndex = i; break; }
                                             }
-                                            console.log("[CW] SingleSelect icon logic - selIndex:", selIndex, "total items:", uiItems.length);
                                             if (selIndex < 0) return;
 
                                             const selId = nodes[selIndex]?.nodeUnique;
